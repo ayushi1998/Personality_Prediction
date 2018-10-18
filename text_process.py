51fb4ee40aa3b3a8ca387490bd11e4d51b9048c4
trinity97 <abhishek16004@iiitd.ac.in>
2018-10-18 15:15:24 +0530

    functions returning some structure which we don't understand

--- text_process.py
+++ text_process.py
@@ -3,71 +3,56 @@ import get_tweets
 import csv
 import re
 import nltk
-"""from nltk.stem.porter import PorterStemmer
-stemmer = PorterStemmer()"""
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from autocorrect import spell
 
-def cleaning(data):
-    from nltk.tokenize import word_tokenize
-    from nltk.corpus import stopwords
+def cleaning(list_of_tweets):
     """ data is a list  """
-    big_data =[];
-    label_big_data = {};
-    for i in range(1,16):
-        # Remove none alphabetic characters.
+    all_tokenized = []
+    for string in list_of_tweets:
+        without_links = re.sub(r"http\S+", ' ', string)
+        without_punct = re.sub('[^A-Za-z]', ' ', without_links)
+        lower_case = without_punct.lower()
 
-        data[0][i] = re.sub(r"http\S+", ' ', data[0][i])
+        """Tokenization and removing stop words from one sentence
+        of the list of strings"""
+        tokenized_data = word_tokenize(lower_case)
 
-        data[0][i] = data[0][i].replace("'","")
+        for word in tokenized_data:
+            if word in stopwords.words('english'):
+                # print(word)
+                tokenized_data.remove(word)
 
-        data[0][i] = re.sub('[^A-Za-z]', ' ', data[0][i])
+        all_tokenized.append(tokenized_data)
 
-        data[0][i]= data[0][i].lower()
-        big_data.append(data[0][i])
-
-
-
-
-    """Tokenization and removing stop words from one sentence of the list of strings"""
-
-
-    tokenized_data = word_tokenize(data[0][i])
-    #nltk.download('stopwords')  #nltk.download('wordnet')
-
-
-    for word in tokenized_data:
-        if word in stopwords.words('english'):
-            print(word)
-            tokenized_data.remove(word)
-    """Returning a list of tokenized 'clean' words"""
-    return tokenized_data;
+    return all_tokenized
 
 
 
-def lemmatization(tweet):
+def lemmatization(list_of_tokens):
 
-    from nltk.stem import WordNetLemmatizer
-    from autocorrect import spell
     """Spell will autocorrect the sms language word to dictionary word"""
     wordnet_lemmatizer = WordNetLemmatizer()
+    lemmatized = []
+
     for j in range(len(tweet)):
-        tweet[j] = wordnet_lemmatizer.lemmatize(spell(tweet[j]))
+        lemmatized.append(wordnet_lemmatizer.lemmatize(spell(list_of_tokens[j])))
 
-    tokenized_data=tweet;
-    print(tokenized_data)
-    return tokenized_data
+    return lemmatized
 
 
-def main():
-    data=[]
-    data = read_file();
-    clean_tweet = cleaning(data)
-    lemma_clean_tweet = lemmatization(clean_tweet)
-    sentence_tweet = " ".join(lemma_clean_tweet)
-    from sklearn.feature_extraction.text import CountVectorizer
-    matrix = CountVectorizer(max_features=2)
-
+def text_process(tweets):
+    for person in tweets:
+        all_tokenized = cleaning(person[1])
+        all_lematized = []
+        for list_of_tokens in all_tokenized:
+            lemmatized = lemmatization(list_of_tokens)
+            all_lematized.append(lemmatized)
+    return all_lematized
 
 if __name__ == "__main__":
     tweets = get_tweets.get_tweets_list('../project/mbti_1.csv')
-    print(tweets)
-    # main()
+    # print(tweets)
+    text_process(tweets)
