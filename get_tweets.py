a5ba1a6564a581aa06b075c9f04fb80ad79a046d
trinity97 <abhishek16004@iiitd.ac.in>
2018-10-18 14:06:24 +0530

    get tweets as list containing [label,list_of_tweets]

---
+++ get_tweets.py
@@ -0,0 +1,32 @@
+import csv
+import linecache
+
+
+def get_tweets_list(filename,number=-1):
+    total_lines = 0
+    with open(filename,'r') as f:
+        for line in f:
+            total_lines += 1
+
+    print(total_lines)
+
+    all_tweets = []
+
+    for i in range(2,total_lines):
+        clean = []
+        line = linecache.getline(filename, i)
+        label = line[:4]
+        line = line[5:]
+        line = line.replace('\"','')
+        line = line.replace('\'','')
+        line = line.replace(",",'')
+        tweets = list(filter(None,line.strip('\n').split("|||")))
+        clean.append(label)
+        clean.append(tweets)
+        all_tweets.append(clean)
+    return all_tweets
+
+
+if __name__ == '__main__':
+    all_tweets = get_tweets_list('./mbti_1.csv')
+    print(all_tweets)
