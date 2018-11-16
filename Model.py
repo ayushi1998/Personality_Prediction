2d71cb66d88f2a810b1bfccce459610e5102d213
Tanish-Gupta <tanish16106@iiitd.ac.in>
2018-11-16 20:38:47 +0530

    Added for each category

--- Model.py
+++ Model.py
@@ -7,49 +7,41 @@ from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def split():
-    X , y = get_bag_of_words('data.dat')
+def split(X, y):
+
     X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2)
     return X_train, X_test, y_train, y_test 
 
 
+def LR(X_train, X_test, y_train, y_test):
 
-def trainLR():
-    X_train, X_test, y_train, y_test  = split()
     lr = LogisticRegression(solver='lbfgs')
-
-    
     lr.fit(X_train, y_train)
     y_pred_class = lr.predict(X_test)
     #y_pred_prob = lr.predict_proba(X_test)[:, 1]
-
     print(lr.score(X_test, y_test))
 
     return lr
 
 
-def trainSVM():
-    X_train, X_test, y_train, y_test  = split()
+def SVM(X_train, X_test, y_train, y_test):
+
     clf = SVC(gamma=0.001, C=1)
     clf.fit(X_train, y_train)
     print(clf.score(X_test, y_test))
  
-
     return clf
 
 
+def KNN(X_train, X_test, y_train, y_test):
 
-
-
-def trainKNN():
-    X_train, X_test, y_train, y_test  = split()
     knn = KNeighborsClassifier(n_neighbors=1000)
     knn.fit(X_train, y_train)
     print(knn.score(X_test, y_test))
     
 
-def RandomForest():
-    X_train, X_test, y_train, y_test  = split()
+def RandomForest(X_train, X_test, y_train, y_test):
+
     random_forest = RandomForestClassifier(n_estimators=100)
     random_forest.fit(X_train, y_train)
 
@@ -65,4 +57,18 @@ def RandomForest():
     
 if __name__ == '__main__':
 
-	trainKNN()
+    X, y = get_bag_of_words('data.dat') #Get processed data and labels from data folder
+
+    y_EI = [l[0] for l in y] #for training for E vs I - stroes 1 bit label
+    y_NS = [l[1] for l in y] #for training for N vs S - stroes 1 bit label
+    y_FT = [l[2] for l in y] #for training for N vs T - stroes 1 bit label
+    y_JP = [l[3] for l in y] #for training for J vs P - stroes 1 bit label
+
+    y_category = [y_EI, y_NS, y_FT, y_JP, y]
+    y_category_names = ['EvsI', 'NvsS', 'FvsT', 'JvsP', 'Overall']
+
+    for i in range(5):
+        X_train, X_test, y_train, y_test = split(X, y_category[i])
+        print(y_category_names[i])
+        SVM(X_train, X_test, y_train, y_test)
+        
