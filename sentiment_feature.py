e91c7eb6f1fff42e063fe543929c6193fbe7ac7c
trinity97 <abhishek16004@iiitd.ac.in>
2018-11-17 12:57:06 +0530

    changed to numpy array

--- sentiment_feature.py
+++ sentiment_feature.py
@@ -1,39 +1,44 @@
 import get_tweets
 from textblob import TextBlob
+import time
+import numpy as np
 
-def feature_extraction(tweets):
+def get_sentiment(tweets):
 
     feature = []
-    l = []
-    #list_of_tweets 
+    labels = []
+    #list_of_tweets
     for person in tweets:
-        l.append(person[0])
+        labels.append(person[0])
         list_of_tweets = person[1]
         polarity_value=[]
-        for sentance in list_of_tweets:
-            test = TextBlob(sentance)
-            polarity_value.append(tes.sentiment.polarity)
+        for sentence in list_of_tweets:
+            test = TextBlob(sentence)
+            polarity_value.append(test.sentiment.polarity)
 
         feature.append(polarity_value)
 
-        
 
-    return feature,l
-            
-            
-            
-        
+
+    return np.array(feature),np.array(labels)
+
+
+
+
 
 if __name__ == '__main__':
     start = time.time()
     tweets = get_tweets.get_tweets_list('../project/mbti_1.csv')
-    X , y = feature_extraction(tweets([0:5])
-    end = time.time()
-    print("Time",end - start)
-    print(X)
-    
 
-        
-        
-        
-    
+    eq_len = []
+    for tweet in tweets:
+        if len(tweet[1])!=50:
+            continue
+        eq_len.append(tweet)
+
+    tweets = eq_len
+    print(len(eq_len))
+    X, y = get_sentiment(tweets[0:20])
+    end = time.time()
+    # print(X)
+    print("Time",end - start)
